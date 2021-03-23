a43cca1a407018eb9ae70e66d5b14f48254cea18
Michael Lande Blau <miclande@gmail.com>
2021-03-23 22:39:38 +0100

    .

---
+++ src/echo.ts
@@ -0,0 +1,9 @@
+export const handler = async function (event: any, context: any) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message:
+        event.queryStringParameters && event.queryStringParameters.message,
+    }),
+  };
+};
